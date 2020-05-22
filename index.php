7d61842c68fba899ec6919a6ed710607a8542ca9
BytesCrafter <bytescrafter@gmail.com>
2020-05-22 14:51:19 +0800

    Fixed the styling on Signin page.

--- index.php
+++ index.php
@@ -12,54 +12,39 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v4.0.1">
-    <title>SIGN IN</title>
+    <title>Demoguy - SignIn</title>
 
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="assets/style.css" type="text/css">
 </head>
 
 <body>
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <form id="signin-form" class="form-signin">
-                    <div class="text-center mb-4">
-                        <h1 class="h3 mb-3 font-weight-normal">SIGN IN</h1>
-                    </div>
+            <div class="col-md-12" style="position: relative;">
+                <div class="form-window">
+                    <form id="signin-form" class="form-signin">
+                        <div class="text-center mb-4">
+                            <h1 class="mb-3 font-weight-normal">DEMOGUY</h1>
+                            <h5 class="mb-3 font-weight-normal">SIGNIN FORM</h5>
+                        </div>
 
-                    <div class="form-label-group">
-                        <label for="inputEmail">Email address</label>
-                        <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                    </div>
+                        <div class="form-label-group">
+                            <label for="inputEmail">Email: </label>
+                            <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                        </div>
 
-                    <div class="form-label-group">
-                        <label for="inputEmail">Password</label>
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                    </div>
+                        <div class="form-label-group">
+                            <label for="inputEmail" style="margin-top: 12px;">Password: </label>
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                        </div>
 
-                    <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 24px;">Sign in</button>
-                    <p class="mt-5 mb-3 text-muted text-center">Not a member? <a href="/register.php">REGISTER</a></p>
-                </form>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 24px;">Sign in</button>
+                        <p class="mt-5 mb-3 text-muted text-center">Not a Member? <a href="/register.php">REGISTER</a></p>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
