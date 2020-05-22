f7e9d0ab884a035ec48f6a012d15312f7b402085
BytesCrafter <bytescrafter@gmail.com>
2020-05-22 14:54:51 +0800

    Fixed styling for register page.

--- register.php
+++ register.php
@@ -6,31 +6,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Bytes Crafter">
     <meta name="generator" content="Jekyll v4.0.1">
-    <title>SIGN IN</title>
+    <title>Demoguy - SignUp</title>
 
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="assets/style.css" type="text/css">
 </head>
 
 <body>
@@ -38,29 +20,31 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <form id="signup-form" class="form-signin">
-                    <div class="text-center mb-4">
-                        <h1 class="h3 mb-3 font-weight-normal">REGISTER</h1>
-                    </div>
+                <div class="form-window">
+                    <form id="signup-form" class="form-signin">
+                        <div class="text-center mb-4">
+                            <h1 class="h3 mb-3 font-weight-normal">REGISTER</h1>
+                        </div>
 
-                    <div class="form-label-group">
-                        <label for="inputEmail">Email address</label>
-                        <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                    </div>
+                        <div class="form-label-group">
+                            <label for="inputEmail">Email address</label>
+                            <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                        </div>
 
-                    <div class="form-label-group">
-                        <label for="inputEmail">Password</label>
-                        <input type="password" id="inputPassword1" class="form-control" placeholder="Password" required>
-                    </div>
+                        <div class="form-label-group">
+                            <label for="inputEmail" style="margin-top: 12px;">Password</label>
+                            <input type="password" id="inputPassword1" class="form-control" placeholder="Password" required>
+                        </div>
 
-                    <div class="form-label-group">
-                        <label for="inputEmail">Confirm</label>
-                        <input type="password" id="inputPassword2" class="form-control" placeholder="Password" required>
-                    </div>
+                        <div class="form-label-group">
+                            <label for="inputEmail" style="margin-top: 12px;">Confirm</label>
+                            <input type="password" id="inputPassword2" class="form-control" placeholder="Password" required>
+                        </div>
 
-                    <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 24px;">Sign Up</button>
-                    <p class="mt-5 mb-3 text-muted text-center">Login <a href="/index.php">HERE</a></p>
-                </form>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 24px;">Sign Up</button>
+                        <p class="mt-5 mb-3 text-muted text-center">Ready? <a href="/index.php">LOGIN</a></p>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
